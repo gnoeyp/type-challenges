b6df2dbc19a9eb345bc74bc276bfd843663c565d
chananababa <ephong93@gmail.com>
2023-03-14 23:04:42 +0900

    03060-easy-unshift

--- scripts/updateReadme.js
+++ scripts/updateReadme.js
@@ -3,17 +3,25 @@ const VerEx = require('verbal-expressions')
 
 
 const replaceColor = (regExp, source) => {
-  const found = source.match(regExp)[0]
-  const splitted = found.split('-')
-  splitted[splitted.length - 1] = 'gray"'
-  const target = splitted.join('-')
-  return regExp.replace(source, target)
+  try {
+    const found = source.match(regExp)[0]
+    const splitted = found.split('-')
+    splitted[splitted.length - 1] = 'gray"'
+    const target = splitted.join('-')
+    return regExp.replace(source, target)
+  } catch {
+    return source
+  }
 }
 
 const replaceLink = (regExp, source, filename) => {
-  const found = source.match(regExp)[0]
-  const target = VerEx().find('./questions/').anythingBut('"').then('"').replace(found, `./solutions/${filename}"`)
-  return regExp.replace(source, target)
+  try {
+    const found = source.match(regExp)[0]
+    const target = VerEx().find('./questions/').anythingBut('"').then('"').replace(found, `./solutions/${filename}"`)
+    return regExp.replace(source, target)
+  } catch {
+    return source
+  }
 }
 
 try {
@@ -21,7 +29,7 @@ try {
   let readmeContent = fs.readFileSync('README.md', 'utf8')
   filenames.forEach(filename => {
     const id = filename.split('-')[0]
-    const regExp = VerEx().find('<a href="./questions/').then(id).anythingBut('>').then('>').maybe(' ').then('<img src="https://img.shields.io/badge/').anythingBut('"').then('"')
+    const regExp = VerEx().find('<a href="./').then('questions').then('/').then(id).anythingBut('>').then('>').maybe(' ').then('<img src="https://img.shields.io/badge/').anythingBut('"').then('"')
     readmeContent = replaceColor(regExp, readmeContent)
     readmeContent = replaceLink(regExp, readmeContent, filename)
   })
