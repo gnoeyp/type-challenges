d7b22d37210d740d9489f1db47bba827a5e3f99b
chananababa <ephong93@gmail.com>
2023-03-14 22:36:58 +0900

    change link

--- scripts/updateReadme.js
+++ scripts/updateReadme.js
@@ -1,16 +1,29 @@
 const fs = require('fs')
 const VerEx = require('verbal-expressions')
 
+
+const replaceColor = (regExp, source) => {
+  const found = source.match(regExp)[0]
+  const splitted = found.split('-')
+  splitted[splitted.length - 1] = 'gray"'
+  const target = splitted.join('-')
+  return regExp.replace(source, target)
+}
+
+const replaceLink = (regExp, source, filename) => {
+  const found = source.match(regExp)[0]
+  const target = VerEx().find('./questions/').anythingBut('"').then('"').replace(found, `./solutions/${filename}"`)
+  return regExp.replace(source, target)
+}
+
 try {
-  const ids = fs.readdirSync('solutions').map(filename => filename.split('-')[0])
+  const filenames = fs.readdirSync('solutions')
   let readmeContent = fs.readFileSync('README.md', 'utf8')
-  ids.forEach(id => {
+  filenames.forEach(filename => {
+    const id = filename.split('-')[0]
     const regExp = VerEx().find('<a href="./questions/').then(id).anythingBut('>').then('>').maybe(' ').then('<img src="https://img.shields.io/badge/').anythingBut('"').then('"')
-    const found = readmeContent.match(regExp)[0]
-    const splitted = found.split('-')
-    splitted[splitted.length - 1] = 'gray"'
-    const target = splitted.join('-')
-    readmeContent = regExp.replace(readmeContent, target)
+    readmeContent = replaceColor(regExp, readmeContent)
+    readmeContent = replaceLink(regExp, readmeContent, filename)
   })
 
   fs.writeFileSync('README.md', readmeContent)
